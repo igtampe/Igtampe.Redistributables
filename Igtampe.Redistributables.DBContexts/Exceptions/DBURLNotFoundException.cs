d0164cffbb538c5af1cb17f52168761641e6ab12
Ignacio Tampe <Igtampe@gmail.com>
2022-08-29 18:58:58 -0400

    Add new DBURL exceptions

---
+++ Igtampe.Redistributables.DBContexts/Exceptions/DBURLNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Igtampe.DBContexts.Exceptions {
+    /// <summary>Exception thrown if the DBURL is not parsable</summary>
+    public class DBURLNotFoundException : Exception {
+
+        /// <summary>Message for this exception</summary>
+        public override string Message => $"Could not find DBURL on Environment Variable or at \"{Path.Combine(Directory.GetCurrentDirectory(),"DBURL.txt")}\"";
+    }
+}
---
+++ Igtampe.Redistributables.DBContexts/Exceptions/DBURLUnparsableException.cs
@@ -0,0 +1,15 @@
+﻿namespace Igtampe.DBContexts.Exceptions {
+    /// <summary>Exception thrown if the DBURL is not parsable</summary>
+    public class DBURLUnparsableException : Exception{
+
+        /// <summary>DBURL that was unparsable</summary>
+        public string DBURL { get; private set; }
+
+        /// <summary>Creates a DBURL Unparsable Exception</summary>
+        /// <param name="DBURL"></param>
+        public DBURLUnparsableException(string DBURL) => this.DBURL = DBURL;
+
+        /// <summary>Message for this exception</summary>
+        public override string Message => $"Database URL \"{DBURL}\" was unparsable!";
+    }
+}
