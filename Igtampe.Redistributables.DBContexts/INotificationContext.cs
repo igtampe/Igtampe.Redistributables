fb4b513279ff1f2cb292e5638acde08a4a1432d1
Ignacio Tampe <Igtampe@gmail.com>
2022-08-29 22:42:48 -0400

    Inotification now accepts any type of notification object

--- Igtampe.Redistributables.DBContexts/INotificationContext.cs
+++ Igtampe.Redistributables.DBContexts/INotificationContext.cs
@@ -5,9 +5,9 @@ using Microsoft.EntityFrameworkCore;
 namespace Igtampe.DBContexts {
 
     /// <summary>A context for objects that are, or are derivative forms of <see cref="Notification"/></summary>
-    public interface INotificationContext<E> : IUserContext<E> where E: User {
+    public interface INotificationContext<E,F> : IUserContext<F> where E: Notification<F> where F : User {
 
         /// <summary>Table of all users</summary>
-        public DbSet<Notification> Notification {get; set;}
+        public DbSet<E> Notification {get; set;}
     }
 }
